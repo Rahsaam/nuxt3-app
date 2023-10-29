d9f6c17df97474ca36368b78e0691e7f4eb572ef
Hesam <he.rahmani@alibaba.ir>
2023-10-29 20:11:43 +0330

    feat: add header and slider

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    app: {
+        head: {
+            htmlAttrs: {
+                dir: 'rtl',
+                lang: 'fa'
+            }
+        }
+    },
+    css: ['~/assets/css/style.css'],
+    devtools: { enabled: true }
+});
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Slider/>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
